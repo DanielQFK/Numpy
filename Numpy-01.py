41d1e2a9b15092d41cdde1a2c0d74d2e953122b8
DanielQFK <DanialQueen2004@gmail.com>
2021-09-23 01:02:27 -0400

    ...

---
+++ Numpy-01.py
@@ -0,0 +1,25 @@
+# Getting start and making array
+# Keywords:np, .array , type
+"""to use numpy library, at first you should install numpy on your system then import it..."""
+import numpy as np
+"""We can create a NumPy ndarray object by using the array() function."""
+array1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(array1)
+"""output:[ 1  2  3  4  5  6  7  8  9 10]"""
+
+# e.g. 2
+array2 = np.array([1 , 2 , 3 , 4 , 5 , 6])
+print(array2)
+print(type(array2)) # to tell us the type of this value
+"""output:
+[1 2 3 4 5 6]
+<class 'numpy.ndarray'>"""
+
+# e.g. 3
+"""tuple or any array-like object into the array() method"""
+array3 = np.array((1 , 2 , 3 , 4 , 5 , 6))
+print(array3)
+print(type(array3))
+"""output:
+[1 2 3 4 5 6]
+<class 'numpy.ndarray'>"""
---
+++ Numpy-02.py
@@ -0,0 +1,33 @@
+# Dimensions in Arrays
+# Keywords: .ndim , ndim='number of D'
+"""examples of different dimensions in numpy """
+"""we can find out the number of D by writting .ndim"""
+import numpy as np
+# D-0
+arrayD_0 = np.array(10)
+print(arrayD_0.ndim)
+"""output:0"""
+
+# D-1
+arrayD_1 = np.array([1, 2, 3, 4, 5, 6])
+print(arrayD_1.ndim)
+"""output:1"""
+
+# D-2
+arrayD_2 = np.array([[1, 2, 3, 4, 5, 6, 7, 8]])
+print(arrayD_2.ndim)
+"""output:2"""
+
+# D-3
+arrayD_3 = np.array([[[1, 2, 3],[4, 5, 6]],[[7, 8, 9],[10, 11, 12]]])
+print(arrayD_3.ndim)
+"""output:3"""
+
+"""Even we can give a dimension to an array"""
+array_giving_D = np.array([1, 2, 3, 4], ndmin=4) #dimesion is 4
+print(array_giving_D)
+print(array_giving_D.ndim)
+"""output:
+[[[[1 2 3 4]]]]
+4
+"""
