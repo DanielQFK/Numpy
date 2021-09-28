1ca319c1b94fa91e85e8f92d8d4d9475f620aba8
Daniel <84660198+DanielQFK@users.noreply.github.com>
2021-09-27 23:22:50 -0400

    Shape of Arrays...

--- Numpy-06.py
+++ Numpy-06.py
@@ -1,4 +1,4 @@
-# Shape of an Array
+# Shape of an Array...
 import numpy as np 
 array1 = np.array([[1, 2, 3, 4, 5],[6, 7, 8, 9, 10],[11, 12, 13, 14, 15],[16, 17, 18, 19, 20]])
 shape1 = array1.shape
@@ -34,4 +34,3 @@ print(array3reshape2)
 [[[ 1  2  3  4  5]
   [ 6  7  8  9 10]]
   """
-
