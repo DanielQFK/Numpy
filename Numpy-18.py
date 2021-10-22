2e240f5cdc0454942dab5c218dd041c68e3e9245
DanielQFK <DanialQueen2004@gmail.com>
2021-10-22 09:19:25 -0400

    ...

---
+++ Numpy-18.py
@@ -0,0 +1,35 @@
+# statistic
+import numpy as np
+array = np.array([[1, 2, 3],[4, 5, 6]])
+print(array)
+"""output:
+[[1 2 3]
+ [4 5 6]]"""
+
+print(np.min(array))
+"""output:1"""
+
+
+print(np.max(array))
+"""output:6"""
+
+print(np.min(array, axis=0))
+"""output:123"""
+
+print(np.max(array, axis=1))
+"""output:36"""
+
+print(np.sum(array))
+"""output:21"""
+
+# reshaping
+print(array.shape)
+"""output:(2, 3)"""
+print(array.reshape((6, 1)))
+"""output:
+[[1]
+ [2]
+ [3]
+ [4]
+ [5]
+ [6]]"""
